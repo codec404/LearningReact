32143a3722df9006f0657d4ca6d64339f84190c5
Saptarshi <saptarshimemari072@gmail.com>
2023-11-29 20:05:52 +0530

    Background Changer

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
     <>
-      <h1>Hello Background Changer</h1>
+      <div className="relative w-full h-screen duration-100" style = {{backgroundColor: color}}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+            <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg text-center' style = {{backgroundColor : "red"}} onClick={() => setColor("red")}>red</button>
+            <button className='outline-none px-4 py-3 rounded-full text-white shadow-lg text-center' style = {{backgroundColor : "green"}}onClick={() => setColor("green")}>green</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style = {{backgroundColor : "blue"}}onClick={() => setColor("blue")}>blue</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style = {{backgroundColor : "purple"}}onClick={() => setColor("purple")}>purple</button>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
